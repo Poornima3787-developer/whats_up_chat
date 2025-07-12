873edad03b759ba808cca60b62d601ea5c0493df
Poornima3787-developer <taehyungpurple02@gmail.com>
2025-07-12 16:37:58 +0530

    Setup the signup page

---
+++ public/signup.js
@@ -0,0 +1,8 @@
+function signupform(event){
+  event.preventDefault();
+
+  const name=event.target.name.value;
+  const email=event.target.email.value;
+  const phoneNumber=event.target.phoneNumber.value;
+  const password=event.target.password.value;
+}
